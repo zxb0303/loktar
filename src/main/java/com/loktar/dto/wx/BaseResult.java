7832bfb754881ab7661276f036a3dbf309929e89
zxb0303 <425291605@163.com>
2024-03-28 09:38:09 +0800

    Update

--- src/main/java/com/loktar/dto/wx/BaseResult.java
+++ src/main/java/com/loktar/dto/wx/BaseResult.java
@@ -5,16 +5,6 @@ import lombok.Data;
 
 @Data
 public class BaseResult {
-
-    public static final int ERRORCODE_SUCCESS = 0;
-
     private int errcode;
     private String errmsg;
-
-    public boolean isSuccess() {
-        if (errcode == ERRORCODE_SUCCESS) {
-            return true;
-        }
-        return false;
-    }
 }
