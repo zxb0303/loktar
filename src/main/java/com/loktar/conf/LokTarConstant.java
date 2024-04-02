78ccf8f5ea40c9e575056ca9d96c6abe92427e23
zxb0303 <48901153+zxb0303@users.noreply.github.com>
2024-04-02 11:32:15 +0800

    Update

---
+++ src/main/java/com/loktar/conf/LokTarConstant.java
@@ -0,0 +1,41 @@
+package com.loktar.conf;
+
+public class LokTarConstant {
+    public static final String HTTP_HEADER_USER_AGENT_NAME = "User-Agent";
+    public static final String HTTP_HEADER_USER_AGENT_VALUE = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36";
+    public static final String HTTP_HEADER_ACCEPT_NAME = "Accept";
+    public static final String HTTP_HEADER_ACCEPT_VALUE_JSON = "application/json";
+    public static final String HTTP_HEADER_ACCEPT_VALUE_HTML = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9";
+
+    public static final String HTTP_HEADER_CONTENT_TYPE_NAME = "Content-type";
+    public static final String HTTP_HEADER_CONTENT_TYPE_VALUE_JSON = "application/json; charset=utf-8";
+    public static final String HTTP_HEADER_CONTENT_TYPE_VALUE_FORM = "application/x-www-form-urlencoded; charset=UTF-8";
+    public static final String HTTP_HEADER_CONTENT_TYPE_VALUE_MULTIPART = "multipart/form-data; boundary=";
+    public static final String HTTP_HEADER_CONTENT_TYPE_VALUE_MULTIPART_PREFIX = "--------------------------";
+    public static final String HTTP_HEADER_ACCEPT_LANGUAGE_NAME = "Accept-Language";
+    public static final String HTTP_HEADER_ACCEPT_LANGUAGE_VALUE_CN = "zh-CN,zh;q=0.9,en;q=0.8";
+
+    public static final String REDIS_KEY_PREFIX_OPENAI_REQUEST = "openai_request_";
+
+    public final static String NOTICE_TITLE_GITHUB = "GitHub项目更新通知";
+    public final static String NOTICE_TITLE_LOTTERY = "杭州新房摇号通知";
+    public final static String NOTICE_TITLE_IP = "家庭宽带IP通知";
+    public final static String NOTICE_TITLE_CAR_VERSION = "沃尔沃车机系统更新通知";
+    public final static String NOTICE_TITLE_WORK = "干活通知";
+    public final static String NOTICE_TITLE_CONTRAC = "合作合同到期通知";
+
+    public final static String NOTICE_TITLE_EMPLOYEE = "劳动合同到期通知";
+
+    public final static String NOTICE_CERT_UPDATE = "证书更新通知";
+    public final static String NOTICE_JELLYFIN = "Jellyfin常规通知";
+    public final static String NOTICE_JELLYFIN_START = "Jellyfin开始播放通知";
+    public final static String NOTICE_JELLYFIN_STOP = "Jellyfin停止播放通知";
+
+    public final static String WX_RECEICE_MSGTYPE = "MsgType";
+    public final static String VOICE_SUFFIX_WAV = ".wav";
+    public final static String VOICE_SUFFIX_AMR = ".amr";
+
+    public final static String ENV_PRO = "pro";
+
+
+}
