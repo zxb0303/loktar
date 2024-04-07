fa99521500079616533dbef534e6c8f78a9d3239
zxb0303 <425291605@163.com>
2024-04-07 11:56:33 +0800

    Update

--- src/main/java/com/loktar/web/github/GithubController.java
+++ src/main/java/com/loktar/web/github/GithubController.java
@@ -1,6 +1,9 @@
 package com.loktar.web.github;
 
+import com.loktar.conf.LokTarConfig;
+import com.loktar.dto.wx.agentmsg.AgentMsgText;
 import com.loktar.service.github.GithubService;
+import com.loktar.util.wx.qywx.QywxApi;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +13,25 @@ public class GithubController {
 
     private final GithubService githubService;
 
-    public GithubController(GithubService githubService) {
+    private final QywxApi QywxApi;
+
+    private final LokTarConfig lokTarConfig;
+
+    public GithubController(GithubService githubService, com.loktar.util.wx.qywx.QywxApi qywxApi, LokTarConfig lokTarConfig) {
         this.githubService = githubService;
+        QywxApi = qywxApi;
+        this.lokTarConfig = lokTarConfig;
     }
 
     @RequestMapping("/check.do")
-    public void check(){
+    public void check() {
         githubService.checkRepositoryTag();
+    }
+
+
+    @RequestMapping("/notifyMsg.do")
+    public void notifyMsg(String version) {
+        QywxApi.sendTextMsg(new AgentMsgText(lokTarConfig.qywxNoticeZxb, lokTarConfig.qywxAgent002Id, version+"已经推送到镜像仓库"));
     }
 
 
