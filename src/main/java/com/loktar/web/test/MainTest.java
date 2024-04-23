90b58bed963f0f5a3de01127e9a4380e37550085
zxb0303 <425291605@163.com>
2024-04-23 11:15:06 +0800

    Update

--- src/main/java/com/loktar/web/test/MainTest.java
+++ src/main/java/com/loktar/web/test/MainTest.java
@@ -1,24 +1,15 @@
 package com.loktar.web.test;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import com.loktar.util.DateTimeUtil;
+
+import java.time.LocalDate;
 
 public class MainTest {
 
     public static void main(String[] args) {
-        // 假设这是从JSON解析得到的ZonedDateTime实例
-        ZonedDateTime zonedDateTime = ZonedDateTime.parse("2024-04-17T08:42:55Z");
-        // 使用系统默认时区进行转换
-        ZonedDateTime localZonedDateTime = zonedDateTime.withZoneSameInstant(ZoneId.systemDefault());
-        // 转换为LocalDateTime
-        LocalDateTime localDateTime = localZonedDateTime.toLocalDateTime();
-
-        // 输出结果
-        System.out.println("ZonedDateTime: " + zonedDateTime);
-        System.out.println("LocalDateTime: " + localDateTime);
-
-
+        String str ="2023-04-01";
+        LocalDate date = DateTimeUtil.parseLocalDate(str, DateTimeUtil.FORMATTER_DATE).plusDays(1);
+        System.out.println("LocalDateTime: " + date);
     }
 
 }
