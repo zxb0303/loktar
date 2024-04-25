bdf6ac745ede1332fe8e3e0663f67f96d998d66c
zxb0303 <425291605@163.com>
2024-04-25 11:23:35 +0800

    Update

--- src/main/java/com/loktar/conf/SecurityConfig.java
+++ src/main/java/com/loktar/conf/SecurityConfig.java
@@ -2,12 +2,10 @@ package com.loktar.conf;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 @EnableWebSecurity
@@ -26,8 +24,6 @@ public class SecurityConfig {
                                 "/test/**"
                         ).permitAll()
                         .anyRequest().authenticated())
-                .exceptionHandling(e -> e
-                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.BAD_REQUEST)))
                 .formLogin(form -> form
                         .defaultSuccessUrl("/swagger-ui/index.html", true)
                         .permitAll());
