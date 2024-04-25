8fc13809d2ef67119a7ec0d1b59904dd6992cb1d
zxb0303 <425291605@163.com>
2024-04-25 10:36:22 +0800

    Update

--- src/main/java/com/loktar/conf/SecurityConfig.java
+++ src/main/java/com/loktar/conf/SecurityConfig.java
@@ -2,9 +2,11 @@ package com.loktar.conf;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 @EnableWebSecurity
@@ -12,13 +14,15 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/jellyfin/**").permitAll()
-                        .requestMatchers("/qywx/callback/chatgpt/**").permitAll()
-                        .requestMatchers("/qywx/callback/**").permitAll()
-                        .requestMatchers("/synology/**").permitAll()
+                        .requestMatchers("/jellyfin/webhook.do").permitAll()
+                        .requestMatchers("/qywx/callback/chatgpt/receive.do").permitAll()
+                        .requestMatchers("/qywx/callback/receive.do").permitAll()
+                        .requestMatchers("/synology/sendMsg.do").permitAll()
                         .requestMatchers("/github/notifyMsg.do").permitAll()
                         .requestMatchers("/test/**").permitAll()
                         .anyRequest().authenticated())
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.BAD_REQUEST)))
                 .formLogin(form -> form
                         .defaultSuccessUrl("/swagger-ui/index.html", true)
                         .permitAll());
