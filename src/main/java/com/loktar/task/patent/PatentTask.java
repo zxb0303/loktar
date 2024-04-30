aded57eed3bb921ccbdeb615762beb7c670b7d0a
zxb0303 <425291605@163.com>
2024-04-30 13:38:01 +0800

    Update Patent

--- src/main/java/com/loktar/task/patent/PatentTask.java
+++ src/main/java/com/loktar/task/patent/PatentTask.java
@@ -1,22 +1,25 @@
 package com.loktar.task.patent;
 
+import com.loktar.conf.LokTarConfig;
 import com.loktar.conf.LokTarConstant;
 import com.loktar.domain.patent.PatentApply;
 import com.loktar.domain.patent.PatentApplyDetail;
 import com.loktar.domain.patent.PatentContent;
 import com.loktar.domain.patent.PatentDetail;
+import com.loktar.dto.wx.agentmsg.AgentMsgText;
 import com.loktar.mapper.patent.PatentApplyDetailMapper;
 import com.loktar.mapper.patent.PatentApplyMapper;
 import com.loktar.mapper.patent.PatentContentMapper;
 import com.loktar.mapper.patent.PatentDetailMapper;
-import com.loktar.util.PatentUtil;
-import com.loktar.util.UUIDUtil;
+import com.loktar.util.*;
+import com.loktar.util.wx.qywx.QywxApi;
 import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Component
@@ -27,19 +30,34 @@ public class PatentTask {
     private final PatentApplyMapper patentApplyMapper;
     private final PatentApplyDetailMapper patentApplyDetailMapper;
     private final PatentContentMapper patentContentMapper;
+    private final QywxApi qywxApi;
+    private final LokTarConfig lokTarConfig;
+    private boolean isProcessing = false;
 
-    public PatentTask(PatentDetailMapper patentDetailMapper, PatentApplyMapper patentApplyMapper, PatentApplyDetailMapper patentApplyDetailMapper, PatentContentMapper patentContentMapper) {
+    public PatentTask(PatentDetailMapper patentDetailMapper, PatentApplyMapper patentApplyMapper, PatentApplyDetailMapper patentApplyDetailMapper, PatentContentMapper patentContentMapper, QywxApi qywxApi, LokTarConfig lokTarConfig) {
         this.patentDetailMapper = patentDetailMapper;
         this.patentApplyMapper = patentApplyMapper;
         this.patentApplyDetailMapper = patentApplyDetailMapper;
         this.patentContentMapper = patentContentMapper;
+        this.qywxApi = qywxApi;
+        this.lokTarConfig = lokTarConfig;
     }
 
 
     @Scheduled(cron = "0 */5 * * * ?")
     public void dealPatent() {
+        if (isProcessing) {
+            String content = LokTarConstant.NOTICE_PATENT_PROCESSIONG + System.lineSeparator()
+                    + System.lineSeparator()
+                    + "定时器时间太短处理不过来了" + System.lineSeparator()
+                    + DateTimeUtil.getDatetimeStr(LocalDateTime.now(), DateTimeUtil.FORMATTER_DATEMINUTE);
+            qywxApi.sendTextMsg(new AgentMsgText(lokTarConfig.getQywx().getNoticeZxb(), lokTarConfig.getQywx().getAgent003Id(), content));
+            return;
+        }
+        isProcessing = true;
         dealContent();
         dealDetail();
+        isProcessing = false;
     }
 
     private void dealContent() {
@@ -57,7 +75,7 @@ public class PatentTask {
         List<PatentDetail> patentDetails = patentDetailMapper.selectByStatus(0);
         for (PatentDetail patentDetail : patentDetails) {
             String applyNameStr = patentDetail.getApplyName();
-            if ("--".equals(applyNameStr)||"信息不完整或未找到".equals(applyNameStr)) {
+            if ("--".equals(applyNameStr) || "信息不完整或未找到".equals(applyNameStr)) {
                 patentDetail.setStatus(1);
                 patentDetailMapper.updateByPrimaryKey(patentDetail);
                 continue;
