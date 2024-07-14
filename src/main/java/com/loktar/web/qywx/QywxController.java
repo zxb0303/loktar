fa281b339adbeb6d7db63363b5a808360c014196
zxb0303 <425291605@163.com>
2024-07-14 19:05:56 +0800

    Update

--- src/main/java/com/loktar/web/qywx/QywxController.java
+++ src/main/java/com/loktar/web/qywx/QywxController.java
@@ -2,7 +2,7 @@ package com.loktar.web.qywx;
 
 import com.loktar.conf.LokTarConfig;
 import com.loktar.dto.wx.UploadMediaRsp;
-import com.loktar.dto.wx.agentmsg.AgentMsgFile;
+import com.loktar.dto.wx.agentmsg.AgentMsgMarkdown;
 import com.loktar.util.wx.qywx.QywxApi;
 import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,7 +24,7 @@ public class QywxController {
     }
 
     @GetMapping("/send.do")
-    public void send(String mediaId) {
+    public void send() {
 
 //        String content = LokTarConstant.NOTICE_TITLE_GITHUB + "\n\n"
 //                + "试试"
@@ -32,8 +32,8 @@ public class QywxController {
 //        qywxApi.sendTextMsg(new AgentMsgText(lokTarConfig.getQywx().getNoticeZxb(), lokTarConfig.getQywx().getAgent006Id(), content));
 //        qywxApi.sendVoiceMsg(new AgentMsgVoice(lokTarConfig.getQywx().getNoticeZxb(), lokTarConfig.getQywx().getAgent003Id(), "3zUEeZmUuc-Eno-3qO9bDgClOpEoEL2XvqTyGpPpOqmrTswb-zG3rzsUOK8IKC5by"));
     //
-        qywxApi.sendFileMsg(new AgentMsgFile(lokTarConfig.getQywx().getNoticeZxb(), lokTarConfig.getQywx().getAgent006Id(), mediaId));
-
+//        qywxApi.sendFileMsg(new AgentMsgFile(lokTarConfig.getQywx().getNoticeZxb(), lokTarConfig.getQywx().getAgent006Id(), mediaId));
+        qywxApi.sendMarkdownMsg(new AgentMsgMarkdown(lokTarConfig.getQywx().getNoticeZxb(), lokTarConfig.getQywx().getAgent006Id(), "您的会议室已经预定，稍后会同步到`邮箱`  \\n>**事项详情**  \\n>事　项：<font color=\\\"info\\\">开会</font>  \\n>组织者：@miglioguan  \\n>参与者：@miglioguan、@kunliu、@jamdeezhou、@kanexiong、@kisonwang  \\n>  \\n>会议室：<font color=\\\"info\\\">广州TIT 1楼 301</font>  \\n>日　期：<font color=\\\"warning\\\">2018年5月18日</font>  \\n>时　间：<font color=\\\"comment\\\">上午9:00-11:00</font>  \\n>  \\n>请准时参加会议。  \\n>  \\n>如需修改会议信息，请点击：[修改会议信息](https://work.weixin.qq.com)" ));
 
 
 
