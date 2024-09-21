3dd01a9c38662fbb1ea922f24c8d3cf5065122b1
zxb0303 <425291605@163.com>
2024-09-21 13:30:49 +0800

    Update

---
+++ src/main/java/com/loktar/mapper/patent/PatentDetailDocInfoMapper.java
@@ -0,0 +1,18 @@
+package com.loktar.mapper.patent;
+
+import com.loktar.domain.patent.PatentDetailDocInfo;
+import java.util.List;
+
+public interface PatentDetailDocInfoMapper {
+    int deleteByPrimaryKey(String docInfoId);
+
+    int insert(PatentDetailDocInfo row);
+
+    PatentDetailDocInfo selectByPrimaryKey(String docInfoId);
+
+    List<PatentDetailDocInfo> selectAll();
+
+    int updateByPrimaryKey(PatentDetailDocInfo row);
+
+    void insertBatch(List<PatentDetailDocInfo> patentDetailDocInfos);
+}
