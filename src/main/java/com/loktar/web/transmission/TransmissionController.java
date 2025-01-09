06fa51ea31f206ca404f84403f0ecf607a9da4b6
zxb <zxb425291605@gmail.com>
2025-01-09 09:52:18 +0800

    Update

--- src/main/java/com/loktar/web/transmission/TransmissionController.java
+++ src/main/java/com/loktar/web/transmission/TransmissionController.java
@@ -1,24 +1,45 @@
 package com.loktar.web.transmission;
 
 import com.loktar.conf.LokTarConfig;
+import com.loktar.domain.transmission.TrRss;
+import com.loktar.service.transmission.RssService;
 import com.loktar.service.transmission.TransmissionService;
+import com.loktar.util.DelayUtil;
 import com.loktar.util.TransmissionUtil;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("transmission")
 public class TransmissionController {
     private final TransmissionService transmissionService;
+    private final RssService rssService;
+
     private final TransmissionUtil transmissionUtil;
     private final LokTarConfig lokTarConfig;
 
-    public TransmissionController(TransmissionService transmissionService, TransmissionUtil transmissionUtil, LokTarConfig lokTarConfig) {
+    public TransmissionController(TransmissionService transmissionService, RssService rssService, TransmissionUtil transmissionUtil, LokTarConfig lokTarConfig) {
         this.transmissionService = transmissionService;
+        this.rssService = rssService;
         this.transmissionUtil = transmissionUtil;
         this.lokTarConfig = lokTarConfig;
     }
+    @GetMapping("/testRss.do")
+    public void testRss() {
+        List<TrRss> trRsss = rssService.getTrRsssByStatus(1);
+        for (TrRss trRss : trRsss) {
+            DelayUtil.delaySeconds(2, 5);
+            //TODO 打印
+            System.out.println("transmission:refreshAndDealTrRssTorrents:每隔" + trRss.getIntervalMinutes() + "分钟执行RSS:" + trRss.getHostCnName());
+            rssService.refreshTrRssTorrents(trRss);
+            rssService.dealTrRssTorrents(trRss);
+
+        }
+    }
+
 
     @GetMapping("/testAdd.do")
     public void testAdd() {
