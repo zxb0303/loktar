39f8759bda567cec7450b2ab8e185ebf1d5ac279
zxb0303 <48901153+zxb0303@users.noreply.github.com>
2025-01-16 11:40:55 +0800

    Update

--- src/main/java/com/loktar/web/patent/PatentSealMain.java
+++ src/main/java/com/loktar/web/patent/PatentSealMain.java
@@ -11,7 +11,7 @@ import java.util.Random;
 
 public class PatentSealMain {
     public static String BASE_FOLD_PATH = "F:/OneDrive/Patent/seal/";
-    public static String CONTRACT_FILE_NAME = "合同";
+    public static String CONTRACT_FILE_NAME = "专利转让协议以及合同";
     public static String AGREEMENT_FILE_NAME = "协议";
     public static String PDF_FILE_SUFFIX = ".pdf";
     public static String NEW_FILE_SUFFX = "-盖章.pdf";
@@ -23,15 +23,15 @@ public class PatentSealMain {
     //骑缝位置
     public static float CONTRACT123_HEIGHT = 100f;
     //合同最后一页位置
-    public static float CONTRACT3_WEIGHT = 100f;
-    public static float CONTRACT3_HEIGHT = 100f;
+    public static float CONTRACT3_WEIGHT = 130f;
+    public static float CONTRACT3_HEIGHT = 200f;
     //协议位置
     public static float AGREEMENT_WEIGHT = 100f;
     public static float AGREEMENT_HEIGHT = 100f;
 
     public static void main(String[] args) {
         createContract();
-        //createAgreement();
+        createAgreement();
     }
 
     @SneakyThrows
@@ -66,7 +66,8 @@ public class PatentSealMain {
             if (pageNumber == numberOfPages) {
                 writeImageSeal0(canvas, CONTRACT3_WEIGHT, CONTRACT3_HEIGHT);
             }
-//            if (pageNumber == 3) {
+            // 只有1个pdf，第1页或者最后1页是协议时
+//            if (pageNumber == 1) {
 //                writeImageSeal0(canvas, CONTRACT3_WEIGHT, CONTRACT3_HEIGHT);
 //            }
 //            if (pageNumber == 4) {
