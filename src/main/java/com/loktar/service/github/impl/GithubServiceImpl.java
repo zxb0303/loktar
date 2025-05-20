0c1d3bfa9836cca5bb063525047392d5aff24942
zxb <48901153+zxb0303@users.noreply.github.com>
2025-05-20 10:55:41 +0800

    fix(github): increase HTTP request timeout from 30 to 60 seconds

--- src/main/java/com/loktar/service/github/impl/GithubServiceImpl.java
+++ src/main/java/com/loktar/service/github/impl/GithubServiceImpl.java
@@ -82,7 +82,7 @@ public class GithubServiceImpl implements GithubService {
         URI uri = URI.create(MessageFormat.format(URL, registory));
         HttpRequest httpRequest = HttpRequest.newBuilder()
                 .uri(uri)
-                .timeout(Duration.ofSeconds(30))
+                .timeout(Duration.ofSeconds(60))
                 .header(LokTarConstant.HTTP_HEADER_USER_AGENT_NAME, LokTarConstant.HTTP_HEADER_USER_AGENT_VALUE)
                 .header(LokTarConstant.HTTP_HEADER_ACCEPT_NAME, LokTarConstant.HTTP_HEADER_ACCEPT_VALUE_JSON)
                 .header("authorization", lokTarConfig.getGithub().getAuthorization())
