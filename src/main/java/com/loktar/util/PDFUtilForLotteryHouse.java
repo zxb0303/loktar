f23f8e4b368e2874337967ebde0f343d2d429075
zxb <48901153+zxb0303@users.noreply.github.com>
2025-07-17 13:05:13 +0800

    chore(PDFUtilForLotteryHouse): refactor PDF parsing logic and add numeric validation method

--- src/main/java/com/loktar/util/PDFUtilForLotteryHouse.java
+++ src/main/java/com/loktar/util/PDFUtilForLotteryHouse.java
@@ -14,13 +14,13 @@ import java.util.*;
 public class PDFUtilForLotteryHouse {
 
     public static List<HZLotteryPeopleDTOV2> getTableContentFromPDFUrl(String pdfUrl, String pdfPath) {
-        int getTableCellsNum = -1;
+//        int getTableCellsNum = -1;
         List<HZLotteryPeopleDTOV2> hZLotteryPeopleDTOV2s = new ArrayList<>();
         String fileFold = PDFBoxUtil.splitPDFFromUrl(pdfUrl, pdfPath);
         File file = new File(pdfPath + fileFold);
         File[] pdfFiles = file.listFiles();
         Arrays.sort(pdfFiles, Comparator.comparing(File::getName));
-
+        int index = 1;
         for (File pdfFile : pdfFiles) {
             try (PDDocument document = Loader.loadPDF(pdfFile)) {
                 PDFTextStripperByArea stripper = new PDFTextStripperByArea();
@@ -42,34 +42,52 @@ public class PDFUtilForLotteryHouse {
                 String result = builder.toString().replaceAll("\n", "").replaceAll("\r", "");
                 String[] strs = result.split(";");
 
-                if (getTableCellsNum == -1) {
-                    getTableCellsNum = 7; // 如果需要动态获取列数，可以实现getTableCells方法
-                    System.out.println("pdf表格列数num:" + getTableCellsNum);
-                    if (getTableCellsNum == 0) {
-                        System.out.println("需要手动处理");
-                        return hZLotteryPeopleDTOV2s;
-                    }
-                }
-
                 for (int i = 0; i < strs.length; i++) {
-                    if (i % getTableCellsNum == 0 && StringUtils.isNumeric(strs[i])) {
+//                    System.out.println(strs[i]);
+                    if (isNumeric(strs[i]) && Integer.valueOf(strs[i]) == index) {
                         HZLotteryPeopleDTOV2 hZLotteryPeopleDTOV2 = new HZLotteryPeopleDTOV2();
                         hZLotteryPeopleDTOV2.setPeopleId(strs[i]);
                         hZLotteryPeopleDTOV2.setSerialNum(strs[i + 1]);
                         hZLotteryPeopleDTOV2.setName(strs[i + 2]);
                         hZLotteryPeopleDTOV2.setIdentityNum(strs[i + 3]);
-                        hZLotteryPeopleDTOV2.setFamilyType(Integer.valueOf(strs[i + 4]));
-                        hZLotteryPeopleDTOV2.setHasOtherPeople(0);
-                        if (i + 5 < strs.length && !StringUtils.isEmpty(strs[i + 5]) && !"/".equals(strs[i + 5])) {
-                            hZLotteryPeopleDTOV2.setHasOtherPeople(1);
-                            hZLotteryPeopleDTOV2.setOtherBuyersName(strs[i + 5]);
-                            if (getTableCellsNum > 6) {
-                                hZLotteryPeopleDTOV2.setOtherBuyersIdnumber(strs[i + 6]);
-                            } else {
-                                hZLotteryPeopleDTOV2.setOtherBuyersIdnumber("");
+
+//                        String identityNum = strs[i + 3];
+//                        String last4 = identityNum.length() >= 4 ? identityNum.substring(identityNum.length() - 4) : identityNum;
+//                        if (last4.contains("*")) {
+//                            identityNum = identityNum + strs[i + 4];
+//                            hZLotteryPeopleDTOV2.setIdentityNum(identityNum);
+//                            i = i + 1;
+//                        }
+                        if (strs[i + 4].equals("X")) {
+                            hZLotteryPeopleDTOV2.setIdentityNum(strs[i + 3] + strs[i + 4]);
+                            i = i + 1;
+                        }
+                        if (!isNumeric(strs[i + 4])) {
+                            hZLotteryPeopleDTOV2.setSerialNum(strs[i + 1] + strs[i + 2]);
+                            i = i + 1;
+                            hZLotteryPeopleDTOV2.setName(strs[i + 2]);
+                            if (strs[i + 4].equals("X")) {
+                                hZLotteryPeopleDTOV2.setIdentityNum(strs[i + 3] + strs[i + 4]);
+                                i = i + 1;
                             }
                         }
+                        hZLotteryPeopleDTOV2.setFamilyType(Integer.valueOf(strs[i + 4]));
+                        hZLotteryPeopleDTOV2.setHasOtherPeople(0);
+                        if (i + 5 < strs.length && !isNumeric(strs[i + 5])) {
+                            if (!StringUtils.isEmpty(strs[i + 5]) && !"/".equals(strs[i + 5])) {
+                                hZLotteryPeopleDTOV2.setHasOtherPeople(1);
+                                hZLotteryPeopleDTOV2.setOtherBuyersName(strs[i + 5]);
+                                if (i + 6 < strs.length) {
+                                    hZLotteryPeopleDTOV2.setOtherBuyersIdnumber(strs[i + 6]);
+                                } else {
+                                    hZLotteryPeopleDTOV2.setOtherBuyersIdnumber("");
+                                }
+                            }
+                        }
+                        i = i + 4;
+                        //System.out.println(hZLotteryPeopleDTOV2.toString());
                         hZLotteryPeopleDTOV2s.add(hZLotteryPeopleDTOV2);
+                        index = index + 1;
                     }
                 }
             } catch (IOException e) {
@@ -106,5 +124,15 @@ public class PDFUtilForLotteryHouse {
             }
         }
         return map;
+    }
+
+    public static boolean isNumeric(String str) {
+        if (str == null || str.isEmpty()) return false;
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
